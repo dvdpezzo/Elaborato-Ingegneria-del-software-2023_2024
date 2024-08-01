df60f89465d92c2e4354ad0b12c0986e0a49e55a
dvdpezzo <d.pezzotti005@studenti.unibs.it>
2024-08-01 12:10:11 +0200

    Setup versione 1

---
+++ src/model/CampoCaratteristico.java
@@ -0,0 +1,8 @@
+package model;
+import java.util.List;
+
+public class CampoCaratteristico {
+
+    private String nomeCampo;
+    private List<Valore> dominioValori;
+}
---
+++ src/model/Categoria.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Categoria {
+
+    private String nome;
+    private String descrizione;
+    private CampoCaratteristico aCampoCaratteristico;
+
+    private FattoreConversione f;
+    
+}
