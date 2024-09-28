89c27d0c9e36bc76a201b2f2311cc7c9c201b591
djmezv2 <f.mazza002@studenti.unibs.it>
2024-09-28 11:09:48 +0200

    Modifica della classe GestioneFileAccesso con aggiunta di metodi utili per l'accesso (credo tutti?) OSS: problema del menu non ancora risolto, metodi ancora da testare

--- src/MenuAccessoUtenti/GestioneFileAccesso.java
+++ src/MenuAccessoUtenti/GestioneFileAccesso.java
@@ -31,9 +31,9 @@ public class GestioneFileAccesso {
     }
 
 
-    /* Controllo sul nome utente quando avviene la registraione*/
-    public boolean ctrlAccesso(String nomeUtente){
-        if(mapConfiguratori.containsKey(nomeUtente)) {
+    /* Controllo se i dati inseriti dall'utente sono corretti*/
+    public boolean ctrlAccesso(String nomeUtente,String pass){
+        if(mapConfiguratori.containsKey(nomeUtente) && mapConfiguratori.containsValue(pass)) {
             return true;
         }
         return false;
@@ -48,16 +48,71 @@ public class GestioneFileAccesso {
         return false;
     }
 
-    /*
-    a)metodo che mi permette di creare un nuovo account dopo l'accesso di default
-    b)metodo che mi permette di gestire il reidirizzamento dopo l'accesso di default
-    c)metodo che mi permatta di aggiornare i dati inseriti dagli utenti.
-    d)metodo che mi verifichi l'accesso
-    e)servirebbero dei metodi set e get per le chiavi e il valore della mappa
+    /* metodo utilizzato per la configurazione iniziale, in quanto mette all'interno della mappa tutti i nomi utenti
+     * e password
+     * OSS: NON ANCORA TESTATO */
 
-     */
+    public void configuraMappa() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file_configuratori));
+        for(HashMap.Entry<String,String> entry : mapConfiguratori.entrySet()){
+            String parola= br.readLine();
+            while(parola!=null && !parola.equals("\n")){
+                String [] dati = parola.split(" ");
+                String nome = dati[0];
+                String pass = dati[1];
+                mapConfiguratori.put(nome,pass);
+
+                parola=br.readLine();
+            }
+        }
+    }
+
+    /* metoto usato per l'accesso del configuratore
+     * OSS: ANCORA DA TESTARE */
+    public void accessoConfig(){
+        String nomeUtente= InputDati.leggiStringaNonVuota("Inserisci il tuo nome utente:");
+        String pass = InputDati.leggiStringaNonVuota("Inserisci la tua password:");
+        if(primoAccesso(nomeUtente,pass)){
+            System.out.println("Sei stasto reindirizzato alla creazione del tuo Nome utente e Password personali:");
+        }
+        else{
+            if(ctrlAccesso(nomeUtente,pass)){
+                System.out.println("Accesso effettuato corretamente!");
+            }
+            else{
+                System.out.println("ERRORE! Nome Utente o password errati!");
+            }
+
+        }
+
+    }
 
 
+    /*metodo che permette la registrazione di un configuratore con controllo sul nome utente
+     * OSS: ANCORA DA TESTARE*/
+    public void registraConfig() {
+        boolean val;
+        do {
+            val = true;
+            String nomeUtente = InputDati.leggiStringaNonVuota("Insersci il nome utente che vuoi utilizzare:");
+            if (ctrlNomeUtente(nomeUtente)) {
+                String pass = InputDati.leggiStringaNonVuota("Inserisci la tua password personale:");
+                aggiungiDati(nomeUtente, pass);
+                val = false;
+            }
+        }while (val);
+    }
 
 
+    /*Controllo sull'unicità del nome utente, se il nome utente non è presente ritorna true e si procede con
+    * la registrazione tramite il metodo registraConfig()
+    OSS: ANCORA DA TESTARE*/
+    private boolean ctrlNomeUtente(String nomeUtente){
+        if(mapConfiguratori.containsKey(nomeUtente))
+        {
+            System.out.println("Nome utente già utilizzato! Scegline un altro!");
+            return false;
+        }
+        return true;
+    }
 }
