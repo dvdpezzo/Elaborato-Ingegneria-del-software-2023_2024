fcd5a2da64056c8ee632948e155983ac0dd975e0
dvdpezzo <d.pezzotti005@studenti.unibs.it>
2024-10-02 10:25:14 +0200

    Modifica test java

--- src/model/Test.java
+++ src/model/Test.java
@@ -1,23 +1,13 @@
 package model;
-
-import java.io.IOException;
-
-import menuAccessoUtenti.GestoreAccesso;
-import menuAccessoUtenti.GestoreFileAccesso;
-import menuAccessoUtenti.MenuAccesso;
+import menuAccessoUtenti.*;
 
 public class Test {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) {        
 
 		GestoreAccesso ga = new GestoreAccesso();
         //test di avvio del menu 
 		MenuAccesso menu1 = new MenuAccesso();
 		menu1.avvio(ga);
-		
-		
-		
-
 
     }
     
